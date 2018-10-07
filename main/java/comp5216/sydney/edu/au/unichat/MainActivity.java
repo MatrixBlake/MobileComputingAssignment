013d39783ac38c0925cd05ff064de36daa8ef05a
Matrix <blakewkz@gmail.com>
2018-10-07 18:47:52 +1100

    19

--- main/java/comp5216/sydney/edu/au/unichat/MainActivity.java
+++ main/java/comp5216/sydney/edu/au/unichat/MainActivity.java
@@ -1,17 +1,30 @@
 package comp5216.sydney.edu.au.unichat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentSender;
+import android.provider.DocumentsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
 
     @Override
@@ -31,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth=FirebaseAuth.getInstance();
         currentUser=mAuth.getCurrentUser();
+        RootRef=FirebaseDatabase.getInstance().getReference();
 
         mToolbar=(Toolbar)findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -48,7 +63,29 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         if(currentUser==null){
             SendUserToLoginActivity();
+        }{
+            VerifyUserExistence();
         }
+    }
+
+    private void VerifyUserExistence() {
+        String currentUserID = mAuth.getCurrentUser().getUid();
+        RootRef.child("Users").child(currentUserID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if((dataSnapshot.child("name").exists())){
+                    Toast.makeText(MainActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
+                }else {
+                    SendUserToSettingsActivity();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
@@ -71,16 +108,69 @@ public class MainActivity extends AppCompatActivity {
         if(item.getItemId()==R.id.main_find_friends_option){
 
         }
+        if(item.getItemId()==R.id.main_create_group_option){
+            RequestNewGroup();
+        }
         return true;
+    }
+
+    private void RequestNewGroup() {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("Enter Group Name: ");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("e.g Coding Cafe");
+        builder.setView(groupNameField);
+
+
+
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupName = groupNameField.getText().toString();
+                if(TextUtils.isEmpty(groupName)){
+                    Toast.makeText(MainActivity.this, "Please write Group Name", Toast.LENGTH_SHORT).show();
+                }else{
+                    CreateNewGroup(groupName);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void CreateNewGroup(final String groupName) {
+        RootRef.child("Groups").child(groupName).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, groupName+" is created successfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void SendUserToLoginActivity() {
         Intent loginIntent =new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
+        finish();
     }
 
     private void SendUserToSettingsActivity() {
         Intent settingsIntent =new Intent(MainActivity.this,SettingsActivity.class);
+        settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingsIntent);
+        finish();
     }
 }
--- main/java/comp5216/sydney/edu/au/unichat/SettingsActivity.java
+++ main/java/comp5216/sydney/edu/au/unichat/SettingsActivity.java
@@ -1,9 +1,25 @@
 package comp5216.sydney.edu.au.unichat;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -12,20 +28,107 @@ public class SettingsActivity extends AppCompatActivity {
     private Button UpdateAccountsSettings;
     private EditText userName,userStatus;
     private CircleImageView userProfileImage;
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth=FirebaseAuth.getInstance();
+        currentUserID=mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
         InitializeFields();
+
+//        userName.setVisibility(View.INVISIBLE);
+
+        UpdateAccountsSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdateSettings();
+            }
+        });
+        
+        RetrieveUserInfo();
     }
+
+
+
 
     private void InitializeFields() {
         UpdateAccountsSettings=(Button)findViewById(R.id.update_settings_button);
         userName=(EditText)findViewById(R.id.set_user_name);
         userStatus=(EditText)findViewById(R.id.set_profile_status);
         userProfileImage=(CircleImageView)findViewById(R.id.set_profile_image);
+    }
 
+    private void UpdateSettings() {
+        String setUserName=userName.getText().toString();
+        String setUserStatus=userStatus.getText().toString();
+
+        if(TextUtils.isEmpty(setUserName)){
+            Toast.makeText(this, "Please write your user name first...", Toast.LENGTH_SHORT).show();
+        }else if(TextUtils.isEmpty(setUserStatus)){
+            Toast.makeText(this, "Please write your user name status...", Toast.LENGTH_SHORT).show();
+        }else{
+            HashMap<String,String> profileMap = new HashMap<>();
+            profileMap.put("uid",currentUserID);
+            profileMap.put("name",setUserName);
+            profileMap.put("status",setUserStatus);
+            RootRef.child("Users").child(currentUserID).setValue(profileMap)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                SendUserToMainActivity();
+                                Toast.makeText(SettingsActivity.this, "Profile updated successfully", Toast.LENGTH_SHORT).show();
+                            }else{
+                                String message = task.getException().toString();
+                                Toast.makeText(SettingsActivity.this, "Error: "+message, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void RetrieveUserInfo() {
+        RootRef.child("Users").child(currentUserID)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.exists() && dataSnapshot.hasChild("name") && dataSnapshot.hasChild("image")){
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+                            String retrieveProfileImage = dataSnapshot.child("image").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrieveStatus);
+                        }else if(dataSnapshot.exists() && dataSnapshot.hasChild("name")){
+                            String retrieveUserName = dataSnapshot.child("name").getValue().toString();
+                            String retrieveStatus = dataSnapshot.child("status").getValue().toString();
+
+                            userName.setText(retrieveUserName);
+                            userStatus.setText(retrieveStatus);
+                        }else{
+//                            userName.setVisibility(View.VISIBLE);
+                            Toast.makeText(SettingsActivity.this, "Please set & update your profile information", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent =new Intent(SettingsActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 }
