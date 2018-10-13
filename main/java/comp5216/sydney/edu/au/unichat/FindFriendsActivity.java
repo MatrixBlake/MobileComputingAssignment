8b2435be6824018d4ea283bf7520e0a9a15ed626
Dong Lu <43484405+ludong622@users.noreply.github.com>
2018-10-13 13:29:33 +1100

    Add search bar

--- main/java/comp5216/sydney/edu/au/unichat/FindFriendsActivity.java
+++ main/java/comp5216/sydney/edu/au/unichat/FindFriendsActivity.java
@@ -10,13 +10,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -27,11 +33,18 @@ public class FindFriendsActivity extends AppCompatActivity {
     private Toolbar mToolBar;
     private RecyclerView FindFriendsRecyclerList;
     private DatabaseReference UsersRef;
+    private Query UsersRef1;
+    private Button searchButton;
+    private EditText userNameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
+
+        userNameInput = (EditText)findViewById(R.id.find_friends_edit_text);
+        searchButton = (Button)findViewById(R.id.search_button);
+
 
         UsersRef=FirebaseDatabase.getInstance().getReference().child("Users");
 
@@ -41,19 +54,29 @@ public class FindFriendsActivity extends AppCompatActivity {
         mToolBar=(Toolbar)findViewById(R.id.find_friends_toolbar);
         setSupportActionBar(mToolBar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Find Friends");
 
 
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = userNameInput.getText().toString();
+                display(username);
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    protected void display(String s) {
+        if (!s.equals("")) {
+            UsersRef1 = UsersRef.orderByChild("name").equalTo(s);
+        }else{
+            UsersRef1 = UsersRef;
+        }
+
         FirebaseRecyclerOptions<Contacts> options =
                 new FirebaseRecyclerOptions.Builder<Contacts>()
-                .setQuery(UsersRef,Contacts.class)
-                .build();
+                        .setQuery(UsersRef1,Contacts.class)
+                        .build();
 
         FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder> adapter=
                 new FirebaseRecyclerAdapter<Contacts, FindFriendViewHolder>(options) {
@@ -89,6 +112,11 @@ public class FindFriendsActivity extends AppCompatActivity {
 
         adapter.startListening();
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        display("");
+    }
 
     public static class FindFriendViewHolder extends RecyclerView.ViewHolder{
         TextView userName,userStatus;
@@ -103,3 +131,4 @@ public class FindFriendsActivity extends AppCompatActivity {
         }
     }
 }
+
