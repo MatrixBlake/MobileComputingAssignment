603bedb71075d6d3d373ca1fcb1e919e5fafc6e2
Dong Lu <43484405+ludong622@users.noreply.github.com>
2018-10-13 14:35:43 +1100

    Update ChatActivity.java

--- main/java/comp5216/sydney/edu/au/unichat/ChatActivity.java
+++ main/java/comp5216/sydney/edu/au/unichat/ChatActivity.java
@@ -98,8 +98,7 @@ public class ChatActivity extends AppCompatActivity {
 
         userImage = (CircleImageView) findViewById(R.id.custom_profile_image);
         userName = (TextView) findViewById(R.id.custom_profile_name);
-        userLastSeen = (TextView) findViewById(R.id.custom_user_last_seen);
-
+       
         SendMessageButton=(ImageButton)findViewById(R.id.send_message_btn);
         MessageInputText = (EditText)findViewById(R.id.input_message);
 
